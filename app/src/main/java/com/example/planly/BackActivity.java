8179f96158168d73f455326ebb53673c922c822f
teba abed elfatah <tebaabed5@gmail.com>
2025-09-27 08:23:23 +0300

    Back next

--- app/src/main/java/com/example/planly/BackActivity.java
+++ app/src/main/java/com/example/planly/BackActivity.java
@@ -1,6 +1,8 @@
 package com.example.planly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class BackActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void gotoMain(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/planly/MainActivity.java
+++ app/src/main/java/com/example/planly/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.planly;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void gotoBack(View view) {
+        Intent i = new Intent(this, BackActivity.class);
+        startActivity(i);
     }
 }
